8280cc7bd6992cf6fb8870a0ad051e2d556a70eb
John Enverga <johnenverga@yahoo.com>
2019-10-30 18:45:05 -0500

    working demo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.session);
-  res.render('index', { title: 'ExpressTrain' });
+  res.render('index', { title: 'Enverga Express' });
 });
 
 // To Be Used Later
